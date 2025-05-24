2d60edc7b4db1d5404788330e43ad72329722668
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2025-05-24 22:06:57 +0100

    confi do vue

--- public/index.php
+++ public/index.php
@@ -20,6 +20,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
